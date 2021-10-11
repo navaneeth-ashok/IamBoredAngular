13add1264f60b616074ecf74b495a3fb65ea31f1
navaneeth_ashok_1 <navaneeth619@gmail.com>
2021-10-11 17:28:46 +0530

    Added more UI styles, added descriptive text under the suggestions

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Track } from '../tracks';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
-  styleUrls: ['./track.component.css']
+  styleUrls: ['./track.component.css'],
 })
 export class TrackComponent implements OnInit {
-
-  constructor() { }
+  @Input('trackData') public trackItem: Track;
+  public urlPoster: SafeResourceUrl;
+  public urlAlbumLink: SafeResourceUrl;
+  public urlTrackLink: SafeResourceUrl;
+  constructor(public sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
+    this.urlPoster = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.trackItem.posterIMG
+    );
+    this.urlAlbumLink = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.trackItem.albumLink
+    );
+    this.urlTrackLink = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://open.spotify.com/embed/track/' + this.trackItem.trackID
+    );
   }
-
 }
